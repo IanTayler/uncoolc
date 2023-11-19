8d2e8d11187074b4839905d7aed11f11ba18d5b8
IanTayler <iangtayler@gmail.com>
2023-11-19 18:16:46 -0300

    Allow reading from file

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,29 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
 #include "./tokenizer.h"
 
-int main() {
+int main(int argc, char *argv[]) {
   std::string s;
-  getline(std::cin, s);
-  TokenStream tokens = tokenize(s);
-  Token token;
-  while (token.type() != TokenType::END) {
-    token = tokens.next();
-    std::cout << "TOKEN: " << token_type_str(token.type()) << " " << token.rep()
-              << std::endl;
+  std::istream *stream = &std::cin;
+  // Not used if reading from stdin
+  std::fstream f;
+
+  if (argc > 1) {
+    std::string stream_name{argv[1]};
+    if (stream_name != "-")
+      f.open(stream_name, std::ios::in);
+    stream = &f;
   }
+  do {
+    getline(*stream, s);
+    TokenStream tokens = tokenize(s);
+    Token token;
+    while (token.type() != TokenType::END) {
+      token = tokens.next();
+      std::cout << "TOKEN: " << token_type_str(token.type()) << " "
+                << token.rep() << std::endl;
+    }
+  } while (s.length() > 0);
 }
