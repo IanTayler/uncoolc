99364ed154b0bd989628e1c2557abb161b3ca370
IanTayler <iangtayler@gmail.com>
2023-11-19 22:31:31 -0300

    Pretty print with column numbers

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,6 @@
+#include <format>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
@@ -19,9 +21,25 @@ int main(int argc, char *argv[]) {
 
   TokenStream tokens = tokenize(stream);
   Token token;
+  const int position_width = 8;
+  const int token_width = 13;
+
+  std::cout << "POSITION "
+            << "|"
+            << "    TOKEN TYPE "
+            << "|"
+            << " STRING" << std::endl;
+  std::cout << "---------"
+            << "|"
+            << "---------------"
+            << "|"
+            << "-------" << std::endl;
+
   while (token.type() != TokenType::END) {
     token = tokens.next();
-    std::cout << token.line() << ": "  << token_type_str(token.type()) << " " << token.rep()
-              << std::endl;
+    std::cout << std::setw(position_width)
+              << std::format("{}:{}", token.line(), token.column()) << " | "
+              << std::setw(token_width) << token_type_str(token.type()) << " | "
+              << token.rep() << std::endl;
   }
 }
