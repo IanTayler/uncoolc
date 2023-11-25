d0ad4e4c99ab3bcad649a52d4d65bc49c5bed24f
IanTayler <iangtayler@gmail.com>
2023-11-25 18:25:15 -0300

    Write debug output to files

--- src/main.cc
+++ src/main.cc
@@ -8,9 +8,100 @@
 #include "token.h"
 #include "tokenizer.h"
 
+struct CliOptions {
+  bool debug_output;
+  std::filesystem::path debug_dir;
+
+  bool verbose;
+};
+
+std::optional<TokenStream> run_tokenizer(std::istream *input,
+                                         std::shared_ptr<SymbolTable> symbols,
+                                         const CliOptions &options) {
+  TokenStream tokens = tokenize(input, symbols);
+
+  std::ostream *output = nullptr;
+  std::fstream out_file;
+
+  if (options.debug_output) {
+    std::filesystem::create_directories(options.debug_dir);
+    out_file.open(options.debug_dir / "tokenizer.log", std::ios::out);
+    output = &out_file;
+  }
+
+  if (output != nullptr) {
+    std::cout << "got here" << std::endl; // TODO remove
+    Token token;
+
+    const int position_width = 8;
+    const int token_width = 13;
+
+    // Print table header
+    *output << "POSITION "
+            << "|"
+            << "   TOKEN TYPE  "
+            << "|"
+            << " STRING" << std::endl;
+    *output << "---------"
+            << "|"
+            << "---------------"
+            << "|"
+            << "---------" << std::endl;
+
+    int opened_comments = 0;
+    bool line_comment = false;
+
+    TokenType type = token.type();
+    while (type != TokenType::END) {
+      token = tokens.next();
+      type = token.type();
+
+      if (type == TokenType::OPEN_COMMENT)
+        opened_comments++;
+
+      // Ignore tokens inside comments and whitespace unless it's verbose mode
+      if (!options.verbose) {
+        if (opened_comments > 0 && type == TokenType::CLOSE_COMMENT) {
+          opened_comments--;
+        } else if (type == TokenType::LINE_COMMENT) {
+          line_comment = true;
+        }
+
+        if (line_comment && type == TokenType::NEW_LINE) {
+          line_comment = false;
+          continue;
+        }
+
+        if (type == TokenType::NEW_LINE || type == TokenType::SPACE ||
+            type == TokenType::END || type == TokenType::OPEN_COMMENT ||
+            type == TokenType::CLOSE_COMMENT ||
+            type == TokenType::LINE_COMMENT) {
+          continue;
+        }
+
+        if (opened_comments > 0 || line_comment) {
+          continue;
+        }
+      }
+
+      *output << std::setw(position_width)
+              << std::format("{}:{}", token.line(), token.column()) << " | "
+              << std::setw(token_width) << token_type_str(type) << " | "
+              << symbols->get_string(token.symbol()) << std::endl;
+    }
+  }
+  if (output != nullptr) {
+    out_file.flush();
+    out_file.close();
+  }
+  return tokens;
+}
+
 int main(int argc, char *argv[]) {
   std::istream *stream = &std::cin;
-  bool show_all = false;
+  bool verbose = false;
+  bool debug = true; // Default to debug mode while we develop
+  std::filesystem::path debug_dir = "./coolc-debug";
 
   // Not used if reading from stdin
   std::fstream f;
@@ -18,10 +109,14 @@ int main(int argc, char *argv[]) {
   while (arg_pos < argc) {
     std::string arg{argv[arg_pos]};
     if (arg == "-v" || arg == "--verbose")
-      show_all = true;
+      verbose = true;
+
+    else if (arg == "--debug")
+      debug = true;
 
     else if (arg != "-") {
       f.open(arg, std::ios::in);
+      debug_dir /= arg;
       stream = &f;
     }
 
@@ -29,63 +124,9 @@ int main(int argc, char *argv[]) {
   }
 
   std::shared_ptr<SymbolTable> symbols = std::make_shared<SymbolTable>();
-  TokenStream tokens = tokenize(stream, symbols);
 
-  Token token;
+  CliOptions options = {
+      .debug_output = debug, .debug_dir = debug_dir, .verbose = verbose};
 
-  const int position_width = 8;
-  const int token_width = 13;
-
-  // Print table header
-  std::cout << "POSITION "
-            << "|"
-            << "   TOKEN TYPE  "
-            << "|"
-            << " STRING" << std::endl;
-  std::cout << "---------"
-            << "|"
-            << "---------------"
-            << "|"
-            << "---------" << std::endl;
-
-  int opened_comments = 0;
-  bool line_comment = false;
-
-  TokenType type = token.type();
-  while (type != TokenType::END) {
-    token = tokens.next();
-    type = token.type();
-
-    if (type == TokenType::OPEN_COMMENT)
-      opened_comments++;
-
-    // Ignore tokens inside comments and whitespace unless it's verbose mode
-    if (!show_all) {
-      if (opened_comments > 0 && type == TokenType::CLOSE_COMMENT) {
-        opened_comments--;
-      } else if (type == TokenType::LINE_COMMENT) {
-        line_comment = true;
-      }
-
-      if (line_comment && type == TokenType::NEW_LINE) {
-        line_comment = false;
-        continue;
-      }
-
-      if (type == TokenType::NEW_LINE || type == TokenType::SPACE ||
-          type == TokenType::END || type == TokenType::OPEN_COMMENT ||
-          type == TokenType::CLOSE_COMMENT || type == TokenType::LINE_COMMENT) {
-        continue;
-      }
-
-      if (opened_comments > 0 || line_comment) {
-        continue;
-      }
-    }
-
-    std::cout << std::setw(position_width)
-              << std::format("{}:{}", token.line(), token.column()) << " | "
-              << std::setw(token_width) << token_type_str(type) << " | "
-              << symbols->get_string(token.symbol()) << std::endl;
-  }
+  run_tokenizer(stream, symbols, options);
 }
