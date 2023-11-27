f7bfcfcb4e9d183d591c5dd21208a64aa6540751
IanTayler <iangtayler@gmail.com>
2023-11-26 21:16:10 -0300

    parser: support VariableNode

--- src/parser.cc
+++ src/parser.cc
@@ -9,11 +9,15 @@ std::unique_ptr<AttributeNode> Parser::parse_attribute() { return nullptr; }
 
 ExpressionPtr Parser::parse_expression() {
   Token token = tokens.next();
-  if (token.type() == TokenType::NUMBER || token.type() == TokenType::STRING) {
+  switch (token.type()) {
+  case TokenType::NUMBER:
+  case TokenType::STRING:
     return std::make_unique<LiteralNode>(token);
+  case TokenType::OBJECT_NAME:
+    return std::make_unique<VariableNode>(token);
+  default:
+    fatal("Could not parse expression", token);
   }
-
-  fatal("Could not parse expression", token);
   return nullptr;
 }
 
